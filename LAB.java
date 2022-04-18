85eb76cd7901cfe4e0d4a0edd93e9828c9c9bf6b
aryalaashu <aryalaashutosh@gmail.com>
2022-04-18 12:04:31 +0545

    QN

---
+++ LAB.java
@@ -0,0 +1,18 @@
+public class LAB {
+    mport java.util.Scanner;
+
+    public class Check_equal {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a Number: ");
+            int a  = scanner.nextInt();
+            System.out.print("Enter second Number: ");
+            int b  = scanner.nextInt();
+            System.out.print("Enter third Number: ");
+            int c  = scanner.nextInt();
+            String check;
+            check = ((a==b)&&(b==c))?"All the Numbers are equal":((a==b)||(b==c)||(a==c))?"Two Numbers are Equal":"None Numbers are Equal";
+            System.out.println(check);
+        }
+    }
+}
