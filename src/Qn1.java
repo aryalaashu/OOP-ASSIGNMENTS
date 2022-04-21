a6c0e0afe21fbd61e6d97a38e8de504c4992392c
aryalaashu <aryalaashutosh@gmail.com>
2022-04-21 11:28:48 +0545

    Edited IF AND SWITCH

--- src/Qn1.java
+++ src/Qn1.java
@@ -382,7 +382,7 @@ import java.util.Scanner;
             } else if (units<=250) {
                 price=25+75+(units-150)*1.2;
             } else {
-                price=25+75+180+(units-250)*1.5;
+                price=25+75+120+(units-250)*1.5;
             }
             double total=price+0.2*price;
             System.out.println("Total price: "+total);
