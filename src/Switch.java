7d680a4f62715a8e57773b130eed1744594cff94
aryalaashu <aryalaashutosh@gmail.com>
2022-04-25 07:35:59 +0545

    updated switch

--- src/Switch.java
+++ src/Switch.java
@@ -48,6 +48,187 @@ class A5Question1 {
         }
     }
 }
+class A5Question4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num1 = input.nextInt();
+
+        switch ((num1 % 5 == 0 && num1 % 11 == 0) ? 1 : 0) {
+            case 1 -> System.out.println("Divisible by 5 and 11!");
+            case 0 -> System.out.println("Not divisible by 5 and 11!");
+        }
+    }
+}
+class A5Question5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter year: ");
+        int year = input.nextInt();
+
+        switch (year%4) {
+            case 0 : switch (year%100) {
+                case 0 :
+                    switch (year % 400) {
+                        case 0 -> System.out.println("Leap Year");
+                        default -> System.out.println("Normal Year");
+                    }
+                    break;
+                default:
+                    System.out.println("Leap Year");
+                    break;
+            }break;
+            default:
+                System.out.println("Normal Year");
+                break;
+        }
+    }
+}
+class A5Question6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the character ");
+        char char1;
+        char1 = Character.toLowerCase(input.next().charAt(0));
+        switch (char1) {
+            case 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' -> System.out.println("The character is an alphabet.");
+            default -> System.out.println("The character is not an alphabet.");
+        }
+    }
+}
+class A5Question8 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter the character ");
+        char char1 ;
+        char1= Character.toLowerCase(input.next().charAt(0));
+        switch (char1) {
+            case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> System.out.println("The character is a digit.");
+            case 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' -> System.out.println("The character is an alphabet.");
+            default -> System.out.println("The character is a special character");
+        }
+    }
+}
+class A5Question9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char upperLower = input.next().charAt(0);
+        char check = Character.toLowerCase(upperLower);
+
+        switch ((upperLower==check)?0:1) {
+            case 1 -> System.out.println("Character is uppercase");
+            case 0 -> System.out.println("Character is lowercase");
+        }
+    }
+}
+class A5Question15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter coefficient of x\u00b2: ");
+        int a = input.nextInt();
+        System.out.println("Enter coefficient of x: ");
+        int b = input.nextInt();
+        System.out.println("Enter constant: ");
+        int c = input.nextInt();
+        double d = (b * b) - (4 * a * c);
+
+        switch (d >= 0 ? 1 : 0) {
+            case 1 -> {
+                double e = Math.sqrt(d);
+                double root1 = (e - b) / (2 * a);
+                double root2 = -(b + e) / (2 * a);
+                System.out.println("Solution 1: " + root1 + "\nSolution 2: " + root2);
+            }
+            case 0 -> System.out.println("Result includes complex number.");
+        }
+    }
+}
+class A5Question16 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter CP: ");
+        int cp = input.nextInt();
+        System.out.print("Enter SP:");
+        int sp = input.nextInt();
+
+        switch (sp > cp ? 0 : 1) {
+            case 1 -> System.out.println("Profit");
+            case 0 -> System.out.println("Loss");
+            default -> System.out.println("Neither profit nor loss");
+        }
+    }
+}
+class A5Question17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter basic salary: ");
+        int salary = input.nextInt();
+
+        double hra, da, grossSalary;
+        int salaryInt = (salary-1)/10000;
+
+        switch (salaryInt) {
+            case 0 -> {
+                hra = 0.2 * salary;
+                da = 0.8 * salary;
+            }
+            case 1 -> {
+                hra = 0.25 * salary;
+                da = 0.9 * salary;
+            }
+            default -> {
+                hra = 0.3 * salary;
+                da = 0.95 * salary;
+            }
+        }
+
+        grossSalary=salary+hra+da;
+
+        System.out.println("Gross salary:  Rs."+grossSalary);
+    }
+}
+class A5Question18 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter total units: ");
+        int units = input.nextInt();
+        double price;
+
+        int unitsInt = units/50;
+        switch (unitsInt) {
+            case 0 -> price=units*0.5;
+            case 1,2 -> price=25+(units-50)*0.75;
+            case 3,4 -> price=25+75+(units-150)*1.2;
+            default -> price=25+75+120+(units-250)*1.5;
+        }
+        double total=price+0.2*price;
+        System.out.println("Total price: "+total);
+    }
+}
+class A5Question19 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter hours: ");
+        int hours = input.nextInt();
+        hours = hours%24;
+        System.out.println("Enter minutes: ");
+        input.nextInt();
+
+        switch (hours) {
+            case 0,1,2,3,4,5,6,7,8,9,10,11 -> System.out.println("AM");
+            case 13,14,15,16,17,18,19,20,21,22,23 -> System.out.println("PM");
+        }
+    }
+}
+class A5Question20 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        System.out.println(-num);
+    }
+}
 
 
 
@@ -65,6 +246,40 @@ class A5Question11 {
             case 2 -> System.out.println("No. of Days: 28");
             default -> System.out.println("Invalid Number!");
         }
+    }
+}
+class A5Question21 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number (a): ");
+        int a = input.nextInt();
+        System.out.println("Enter another number (b): ");
+        int b = input.nextInt();
+        int temp;
+
+        switch (a == b ? 0 : 1) {
+            case 1 -> {
+                temp = b;
+                b = a;
+                a = temp;
+                System.out.println("Swapped value of a: " + a);
+                System.out.println("Swapped value of b: " + b);
+            }
+            case 0 -> System.out.println("Numbers are equal!");
+        }
+    }
+}
+class A5Question22 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num,x;
+        System.out.print("Enter the number ");
+        num= input.nextInt();
+        x= num%2;
+        switch (x) {
+            case 0 -> num += 1;
+        }
+        System.out.println(num);
     }
 }
 
